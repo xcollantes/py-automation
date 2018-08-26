dc464f37aaf591e6c9a334f7e2c33dfcb6778443
xcollantes <xcollantes@users.noreply.github.com>
2018-08-25 21:50:45 -0700

    Upgrade pip

--- Main.py
+++ Main.py
@@ -3,8 +3,24 @@
 # Examples tried out from Al Sweigart's book, "Automating the Boring Stuff with Python"
 # Web Scraping: Chapter 11
 
-import webbrowser
+import webbrowser, sys, pyperclip 
 
-if __name__ == "__main__":
-	print ("W")
+# Launches Google Maps in browser from command line argument 
+# or clipboard if no command line argument found. 
+
+def mapIt():
+	if len(sys.argv) > 2:
+		address = ' '.join(sys.argv[1:])
+		
+	else:
+		print ("From clipboard")
+		address = pyperclip.paste()
 	
+	print(address)
+	print(type(address))
+	link = "https://www.google.com/maps/place/" + address
+	webbrowser.open(link)
+	
+	
+if __name__ == "__main__":
+	mapIt()
