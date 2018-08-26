a5afb6f5cde29821362af337f68a51bdc20a08f3
xcollantes <xcollantes@users.noreply.github.com>
2018-08-25 21:10:12 -0700

    Added webbroser lib

--- Main.py
+++ Main.py
@@ -1,9 +1,10 @@
 # Xavier Collantes
 # 08/25/18
 # Examples tried out from Al Sweigart's book, "Automating the Boring Stuff with Python"
+# Web Scraping: Chapter 11
 
+import webbrowser
 
-def main():
-    print("AMDG")
-
-main()
+if __name__ == "__main__":
+	print ("W")
+	
