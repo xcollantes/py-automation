a163efea4395df9e34509a3dae85cb9020e5b5f0
xcollantes <xcollantes@users.noreply.github.com>
2018-08-25 17:12:44 -0700

    Add cmd and Python

---
+++ Main.py
@@ -0,0 +1,9 @@
+# Xavier Collantes
+# 08/25/18
+# Examples tried out from Al Sweigart's book, "Automating the Boring Stuff with Python"
+
+
+def main():
+    print("AMDG")
+
+main()
---
+++ WIN_RUN.cmd
@@ -0,0 +1,6 @@
+
+@echo off
+python C:\Users\Xavier\OneDrive\Documents\LocalGit\py-automation\Main.py
+pause
+cls
+WIN_RUN.cmd
