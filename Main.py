e27574cc2623538795aeec5b0d9562886099c239
xcollantes <xcollantes@users.noreply.github.com>
2018-08-26 16:18:49 -0700

    Added toFile

--- Main.py
+++ Main.py
@@ -3,7 +3,7 @@
 # Examples tried out from Al Sweigart's book, "Automating the Boring Stuff with Python"
 # Web Scraping: Chapter 11
 
-import webbrowser, sys, pyperclip, requests
+import webbrowser, sys, pyperclip, requests, pprint, os
 
 # Launches Google Maps in browser from command line argument 
 # or clipboard if no command line argument found. 
@@ -21,6 +21,41 @@ def mapIt():
 	linkMap = "https://www.google.com/maps/place/" + address
 	webbrowser.open(linkMap)
 	
+
+def toFile():
+	linkFed = "https://www.gutenberg.org/files/1404/1404-8.txt"
+	Fpath = "./lib/fed_papers.txt"
+	Dpath = "./lib/"
+	chunkSize = 100000
+	
+	if not os.path.exists(Dpath):
+		print("Error with finding destination file.  Creating path... ")
+		os.makedirs(Dpath)
+	
+	try:
+		response = requests.get(linkFed)
+		response.raise_for_status()
+	except Exception as e:
+		print ("Error loading file: %s" % e)
+	
+	try:
+		file = open(Fpath, 'wb')
+		chunkCount = 0
+		bookLength = 0
+		for chunk in response.iter_content(chunkSize):
+			print (chunk)
+			
+			file.write(chunk)
+			chunkCount += 1
+			bookLength += len(chunk)
+			print("CHUNK COUNT: %s" %chunkCount)
+			print("CHAR LEN: %s" % bookLength)
+			print("PER CHUNK: %s" %(bookLength/chunkCount))
+	except IOError as ioe:
+		print("Error writing to file: %s" % ioe)
+
+	file.close()
+	
 	
 def scrape():
 	linkAli = "https://www.gutenberg.org/files/1009/1009-8.txt"
@@ -33,11 +68,19 @@ def scrape():
 		print("Problem connecting: %s" % e)
 		
 	print (response.text[1050:1500])
-		
-if __name__ == "__main__":
-	program = sys.argv[1]
-	defList = {'mapIt':mapIt, 'scrape':scrape}
+
 	
-	for k, v in defList.items():
-		if k.upper() == program.upper():
-			v()
+	
+	
+if __name__ == "__main__":
+	defList = {'mapIt':mapIt, 'toFile':toFile, 'scrape':scrape}
+	
+	try:
+		program = sys.argv[1]	
+		for k, v in defList.items():
+			if k.upper() == program.upper():
+				v()
+		
+	except IndexError as e:
+		print("No program named.  Please name one of the following: ")
+		print(defList.keys())
